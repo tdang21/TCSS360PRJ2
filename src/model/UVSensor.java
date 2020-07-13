5b5fbee2ed51eaf916cd8df24012e4d52f848361
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2020-07-12 20:56:33 -0700

    Create UVSensor.java

---
+++ src/model/UVSensor.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.Random;
+
+/**
+ *  The sensor that returns the radiation dose.
+ *  
+ * @author Haoying Li
+ * @version 7/12/2020
+ */
+public class UVSensor extends AbstractSensor {
+
+    private double myRadiationDose;
+    private final double MAXRADIATIONDOSE = 199;
+    
+    private Random rand = new Random();
+    
+    /** Constructor for an UVSensor.*/
+    public TemperatureSensor() {
+        updateInterval = 50000; // refresh rate 10s
+        refreshData();
+    }
+    
+    @Override
+    public String getData() {
+        return String.valueOf(myRadiationDose);
+    }
+
+    @Override
+    public void refreshData() {
+        setRadiationDose(); 
+    }
+    
+    public void setRadiationDose() {
+        myTemperature = MAXRADIATIONDOSE * rand.nextDouble();
+    }
+    
+    public double getRadiationDose() {
+        return myRadiationDose;
+    }
+}
